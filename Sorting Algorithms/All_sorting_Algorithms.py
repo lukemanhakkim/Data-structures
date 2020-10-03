afed23783d780e014accfd38b242e672782f9789
S.Lukeman Hakkim <lukemanhakkim@gmail.com>
2020-10-03 13:33:58 -0400

    Added support for Selection sort
    Added support for Selection sort

---
+++ Sorting Algorithms/All_sorting_Algorithms.py
@@ -0,0 +1,45 @@
+"""
+Author: Lukeman Hakkim Sheik Alavudeen
+Description: All sorting algorithms
+
+1. Bubble sort
+	Time complexity: Avg case - O(n2), Best case - O(n)
+2. Selection sort
+	Time complexity: Avg case - O(n2), Best case - O(n)
+"""
+
+#######################
+#Bubble sort
+#######################
+
+def bubble_sort(array):
+    flag=True
+    while flag:
+        flag=False
+        for i in range(len(array)-1):
+            if array[i]>array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                flag=True
+    return array
+
+########################
+#Selection sort
+########################
+def selection_sort(array):
+    
+    for i in range(len(array)):
+        min=i
+        for j in range(i+1, len(array)):
+            if array[j]<array[min]:
+                array[j], array[min]=array[min], array[j]              
+    return array
+    
+
+
+if __name__ == '__main__':
+	
+	array = [5,7,77,1,3,4,9]
+	s1 = bubble_sort(array)
+	s2 = selection_sort(array)
+	print(s1)                
+	print(s2)
